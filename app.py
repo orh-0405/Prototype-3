e243897271ae10854b128db386d410e762363752
Your Name <you@example.com>
2021-07-30 08:01:21 +0800

    finished up 3 pages

--- app.py
+++ app.py
@@ -21,6 +21,10 @@ def about():
 def survey_start():
     return render_template('1_start page.html')
 
+@app.route('/contact_us/')
+def contact_us():
+    return render_template("contact_us.html")
+
 
 @app.route('/survey_page/')
 def survey_page():
